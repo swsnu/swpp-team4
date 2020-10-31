b01bda2c3e834ae9665dee3c049fd271dde40304
ChanHyup Kim <52435465+ChanHyup98@users.noreply.github.com>
2020-10-31 17:31:31 +0900

    Ch 17 sand box (#23)
    * add pandas as dependency
    
    * add cache files and coverage files to gitignore
    
    * delete pycache and add qc_api to setting
    
    * add default data models: kospi, kosdaq, csv_stock

---
+++ back/qc_api/models/__init__.py
@@ -0,0 +1,5 @@
+from django.contrib.auth.models import User
+
+from qc_api.models.default_dataset.kospi import Kospi
+from qc_api.models.default_dataset.kosdaq import Kosdaq
+from qc_api.models.default_dataset.stock_data import StockData
