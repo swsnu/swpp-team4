91cc487851d7487685546dcf63b59d8c1bcd59bd
ChanHyup98 <rlacksque@hotmail.co.kr>
2020-11-06 00:14:31 +0900

    implement snippet polymorphic classes

--- back/qc_api/models/__init__.py
+++ back/qc_api/models/__init__.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 
-from qc_api.models.default_dataset.kospi import Kospi
-from qc_api.models.default_dataset.kosdaq import Kosdaq
-from qc_api.models.default_dataset.stock_data import StockData
+from .default_dataset.kospi import Kospi
+from .default_dataset.kosdaq import Kosdaq
+from .default_dataset.stock_data import StockData
+from .algorithm.snippet import Snippet, SnippetAmount, SnippetBuy, SnippetScope, SnippetSell
