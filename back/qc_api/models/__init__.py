b58a18734185ce51b978ecd0056aecaccb4639ce
ChanHyup Kim <52435465+ChanHyup98@users.noreply.github.com>
2020-11-06 00:31:15 +0900

    Ch 33 snippet model (#46)
    * implement snippet polymorphic classes
    
    * delete csv files

--- back/qc_api/models/__init__.py
+++ back/qc_api/models/__init__.py
@@ -3,3 +3,5 @@ from django.contrib.auth.models import User
 from .default_dataset.kospi import Kospi
 from .default_dataset.kosdaq import Kosdaq
 from .default_dataset.stock_data import StockData
+from .algorithm.snippet import Snippet, SnippetAmount, SnippetBuy, SnippetScope, SnippetSell
+
