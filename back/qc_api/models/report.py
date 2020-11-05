16848a67c35a49fd93b5b9bf3a944e7ba300172c
hwarim <polarcoke2@snu.ac.kr>
2020-11-05 21:56:38 +0900

    initial commit in report model

---
+++ back/qc_api/models/report.py
@@ -0,0 +1,39 @@
+from django.db import models
+
+
+class Report(models.Model):
+    class BackTestStatus(models.TextChoices):
+        PENDING = 'PEND'  # , _('Pending')
+        EXECUTING = 'EXEC'  # , _('Executing')
+        DONE = 'DONE'  # , _('Done')
+        FAILED = 'FAIL'  # , _('Failed')
+        CANCELED = 'QUIT'  # , _('Canceled')
+
+    def has_started(self):  # dummy method to show usage
+        return self.status != self.BackTestStatus.PENDING
+
+    # algorithm = models.ForeignKey()
+
+    # statistics
+    alpha = models.FloatField()
+    profit = models.FloatField()
+    MDD = models.FloatField()
+    optional_stat = models.TextField()
+
+    # backtest settings
+    created_at = models.DateTimeField()
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+
+    transaction_log = models.TextField()
+    initial_budget = models.IntegerField()
+
+    # status
+    status = models.CharField(
+        max_length=4,
+        choices=BackTestStatus.choices,
+        default=BackTestStatus.PENDING,
+    )
+
+    def __str__(self):  # dummy
+        return "report"
