9f66c89563a873cb3be8e849f92caed78b5a2b77
ChanHyup98 <rlacksque@hotmail.co.kr>
2020-11-06 15:05:39 +0900

    add serializer class for individual snippet subclasses

--- back/qc_api/serializers/snippet/snippet_serializer.py
+++ back/qc_api/serializers/snippet/snippet_serializer.py
@@ -1,11 +1,38 @@
 from rest_framework import serializers
-from ...models import Snippet
+from ...models import SnippetScope, SnippetAmount, SnippetBuy, SnippetSell
 
 
-class SnippetSerializer(serializers.ModelSerializer):
-    """Serializer class for snippets"""
+class SnippetScopeSerializer(serializers.ModelSerializer):
+    """Serializer class for snippet scope"""
 
     class Meta:
-        model = Snippet
+        model = SnippetScope
+        fields = ['code', 'name', 'author', 'is_shared']
+        read_only_fields = ('create_at', 'update_at')
+
+
+class SnippetBuySerializer(serializers.ModelSerializer):
+    """Serializer class for snippet buy"""
+
+    class Meta:
+        model = SnippetBuy
+        fields = ['code', 'name', 'author', 'is_shared']
+        read_only_fields = ('create_at', 'update_at')
+
+
+class SnippetSellSerializer(serializers.ModelSerializer):
+    """Serializer class for snippet sell"""
+
+    class Meta:
+        model = SnippetSell
+        fields = ['code', 'name', 'author', 'is_shared']
+        read_only_fields = ('create_at', 'update_at')
+
+
+class SnippetAmountSerializer(serializers.ModelSerializer):
+    """Serializer class for snippet sell"""
+
+    class Meta:
+        model = SnippetAmount
         fields = ['code', 'name', 'author', 'is_shared']
         read_only_fields = ('create_at', 'update_at')
