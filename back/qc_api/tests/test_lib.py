6dc87652a1610808bf439975ec303fe87964eaa9
hwarim <polarcoke2@snu.ac.kr>
2020-11-06 14:48:38 +0900

    lint dataset

--- back/qc_api/tests/test_lib.py
+++ back/qc_api/tests/test_lib.py
@@ -1,4 +1,5 @@
-from django.test import TestCase, Client
+from django.test import TestCase
+
 from ..lib import SandBox
 from ..models import Kospi
 from ..util.utility import parse_date
@@ -9,7 +10,8 @@ class SandBoxTestCase(TestCase):
         pass
 
     def test_sandbox_backtest(self):
-        stub_kospi = Kospi(date='2020-10-10',close=2300,open=2290,high=2301,low=2280,volume=10000000,d1_diff_rate=0.63)
+        stub_kospi = Kospi(date='2020-10-10', close=2300, open=2290, high=2301, low=2280, volume=10000000,
+                           d1_diff_rate=0.63)
         stub_kospi.save()
         sandbox = SandBox(1000, parse_date('2020-10-9'), parse_date('2020-10-11'), 0)
         self.assertEqual(sandbox.budget, 1000)
