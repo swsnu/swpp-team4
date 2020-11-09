b256617c15fda5e18457681a8d91a21dbc5a40b8
ChanHyup98 <rlacksque@hotmail.co.kr>
2020-11-10 04:09:28 +0900

    add relational field representation in algorithm serializer

--- back/qc_api/serializers/algorithm/algorithm_serializer.py
+++ back/qc_api/serializers/algorithm/algorithm_serializer.py
@@ -1,11 +1,32 @@
 from rest_framework import serializers
-from ...models import Snippet, SnippetScope, SnippetAmount, SnippetBuy, SnippetSell, Algorithm
+from ...models import Algorithm
+from ...serializers import SnippetScopeSerializer, SnippetSellSerializer, SnippetBuySerializer, SnippetAmountSerializer
+from typing import Dict, Any
 
 
 class AlgorithmSerializer(serializers.ModelSerializer):
     """Serializer class for Algorithm"""
+    snippet_scope_data = serializers.SerializerMethodField()
+    snippet_sell_data = serializers.SerializerMethodField()
+    snippet_buy_data = serializers.SerializerMethodField()
+    snippet_amount_data = serializers.SerializerMethodField()
+
     class Meta:
         model = Algorithm
         fields = ['name', 'description', 'author', 'is_public', 'snippet_scope', 'snippet_sell', 'snippet_buy',
-                  'snippet_amount', 'create_at', 'update_at']
-        read_only_fields = ('create_at', 'update_at')
+                  'snippet_amount', 'create_at', 'update_at', 'snippet_scope_data', 'snippet_sell_data',
+                  'snippet_buy_data', 'snippet_amount_data']
+        read_only_fields = ('create_at', 'update_at', 'snippet_scope_data', 'snippet_sell_data',
+                            'snippet_buy_data', 'snippet_amount_data')
+
+    def get_snippet_scope_data(self, obj: Algorithm) -> Dict[str, Any]:
+        return SnippetScopeSerializer(obj.snippet_scope).data
+
+    def get_snippet_sell_data(self, obj):
+        return SnippetSellSerializer(obj.snippet_sell).data
+
+    def get_snippet_buy_data(self, obj):
+        return SnippetBuySerializer(obj.snippet_buy).data
+
+    def get_snippet_amount_data(self, obj):
+        return SnippetAmountSerializer(obj.snippet_scope).data
