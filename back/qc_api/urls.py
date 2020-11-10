0b753568ac6f7b69fe1a709c8be41e6bfb0863e0
ChanHyup Kim <52435465+ChanHyup98@users.noreply.github.com>
2020-11-11 03:23:47 +0900

    Ch me apis (#72)
    * add algo/me api
    
    * add algo/me endpoint to urls
    
    * add snippet/me endpoint

--- back/qc_api/urls.py
+++ back/qc_api/urls.py
@@ -7,9 +7,11 @@ urlpatterns = [
     path('sign_up', views.sign_up, name='sign_up'),
     path('sign_out', views.sign_out, name='sign_out'),
     path('snippet', views.get_or_post_snippets, name='snippet'),
+    path('snippet/me', views.get_my_snippets, name='my_snippets'),
     path('algo', views.get_or_post_algorithms, name='algo'),
     path('algo/backtest', views.run_backtest, name='backtest'),
     path('algo/<int:algo_id>/report', views.get_reports_by_algo, name='report'),
     path('algo/<int:algo_id>/performance', views.get_performance_by_algo, name='performance'),
+    path('algo/me', views.get_my_algorithms, name='my_algorithms'),
     path('performance', views.get_all_performances, name='all-performances')
 ]
