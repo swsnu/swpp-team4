07c0b68c485c04c21770680e37f63939513c0bd1
ChanHyup98 <rlacksque@hotmail.co.kr>
2020-11-10 17:12:07 +0900

    implement set universe

--- back/qc_api/lib/backtest/backtester.py
+++ back/qc_api/lib/backtest/backtester.py
@@ -10,10 +10,10 @@ class BackTester:
     """The BackTester Class in which all backtesting functions are defined"""
 
     def __init__(self, algorithm, budget):
-        self.snippet_scope = algorithm.snippet_scope_data.code
-        self.snippet_sell = algorithm.snippet_sell_data.code
-        self.snippet_buy = algorithm.snippet_buy_data.code
-        self.snippet_amount = algorithm.snippet_amount_data.code
+        self.snippet_scope = algorithm.get("snippet_scope_data").get("code")
+        self.snippet_sell = algorithm.get("snippet_sell_data").get("code")
+        self.snippet_buy = algorithm.get("snippet_buy_data").get("code")
+        self.snippet_amount = algorithm.get("snippet_amount_data").get("code")
         self.budget = budget
         self.wallet = Wallet(budget=self.budget)
         self.universe = None
@@ -36,4 +36,9 @@ class BackTester:
     def set_date(self, date: str) -> None:
         """Sets self.today"""
         self.today = parse_date(date)
+
+    def set_universe(self) -> None:
+        """Brings today (in context of backtesting)'s stock data from StockData DB table into memory"""
+        self.universe = pd.DataFrame(list(StockData.objects.filter(date=self.today).values()),
+                                     columns=stock_data_columns)
 
