7fde929988b0f74c0d74c577e3041bd5bfe38579
Hwarim <43122460+polarcoke2@users.noreply.github.com>
2020-11-14 04:06:48 +0900

    [Test][Backend]Testcases for Report/Performance model and view (#78)
    * added tests for evaluation model and view
    
    * performance meta class
    
    * add some lintings
    
    * adding coveralls

--- back/qc_api/tests/utils.py
+++ back/qc_api/tests/utils.py
@@ -1,47 +1,82 @@
+""" Mocking functions for tests. """
+import json
 from enum import Enum
+
+from django.contrib.auth.models import User
 from django.test import Client
 
-import json
-
 from qc_api.models import Algorithm
-from qc_api.models.algorithm.snippet import *
+from qc_api.models.algorithm.snippet import Snippet, SnippetScope, SnippetBuy, SnippetSell, SnippetAmount
 
 
 class SnippetType(Enum):
+    """ Enum for Snippet Type """
     SCOPE = 1
     SELL = 2
     BUY = 3
     AMOUNT = 4
 
 
-def get_signed_in_client() -> Client:
+def get_signed_up_user(username: str, password: str) -> User:
+    """
+    Creates an user object with the given username and password, saves it in the db, and returns it.
+    Parameters:
+        username: user mame string
+        password: password string
+    Returns:
+        User object which is signed-up to the service, but not yet logged-in.
+    """
+    user = User.objects.create(username=username)
+    user.set_password(password)
+    user.save()
+    return user
+
+
+def get_signed_in_client(username: str, password: str) -> Client:
+    """
+    Creates a user with given username and password, saves the user in database,
+    and returns a client which is logged-in as that user.
+    Parameters:
+        username: user mame string
+        password: password string
+    Returns:
+        Signed-in client object
+    """
+    get_signed_up_user(username, password)
     client = Client()
     client.post('/api/sign_in',
-                json.dumps({'username': 'user', 'password': "pwd"}),
+                json.dumps({'username': username, 'password': password}),
                 content_type='application/json')
     return client
 
 
-def get_signed_in_user(userId: int) -> User:
-    user1 = User.objects.create(username=f"user_{userId}")
-    user1.set_password(f'pwd_{userId}')
-    user1.save()
+def get_signed_in_user(username: str, password: str) -> User:
+    """
+    Creates a user with given username and password, saves the user in database,
+    and returns a client which is logged-in as that user.
+    Parameters:
+        username: user mame string
+        password: password string
+    Returns:
+        Signed-in user object
+    """
+    user = get_signed_up_user(username, password)
 
     client = Client()
     client.post('/api/sign_in',
-                json.dumps({'username': f'user_{userId}', 'password': f"pwd_{userId}"}),
+                json.dumps({'username': username, 'password': password}),
                 content_type='application/json')
-    return user1
-
-
-def get_unsigned_in_user(userId: int) -> User:
-    user1 = User.objects.create(username=f"user_{userId}")
-    user1.set_password(f'pwd_{userId}')
-    user1.save()
-    return user1
+    return user
 
 
 def get_mock_snippet(snippet_type: SnippetType) -> Snippet:
+    """
+    Given a snippet type, returns a snippet object
+    Parameters:
+        snippet_type: Snippet Type as enum
+    Returns:
+        Snippet object
+    """
     if snippet_type == SnippetType.SCOPE:
         snippet = SnippetScope.objects.create(name=snippet_type, code=snippet_type)
     elif snippet_type == SnippetType.BUY:
@@ -53,11 +88,15 @@ def get_mock_snippet(snippet_type: SnippetType) -> Snippet:
     return snippet
 
 
-def get_mock_algo() -> Algorithm:
+def get_mock_algo(name: str, public: bool = False) -> Algorithm:
+    """
+    Returns:
+        algo: Algorithm model object.
+    """
     scope = get_mock_snippet(SnippetType.SCOPE)
     buy = get_mock_snippet(SnippetType.BUY)
     sell = get_mock_snippet(SnippetType.SELL)
     amount = get_mock_snippet(SnippetType.AMOUNT)
-    algo = Algorithm.objects.create(name="algo", snippet_scope=scope, snippet_buy=buy,
-                                    snippet_sell=sell, snippet_amount=amount)
+    algo = Algorithm.objects.create(name=name, snippet_scope=scope, snippet_buy=buy,
+                                    snippet_sell=sell, snippet_amount=amount, is_public=public)
     return algo
