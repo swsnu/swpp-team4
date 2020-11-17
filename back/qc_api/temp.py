54c4b360077e7db5979da9584728cf0e945239c6
ChanHyup98 <rlacksque@hotmail.co.kr>
2020-11-17 15:07:27 +0900

    delete unnecessary files

--- back/qc_api/temp.py
+++
@@ -1,9 +0,0 @@
-import os
-import django
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'qc_back.settings')
-django.setup()
-from back.qc_api.models import Kospi
-
-
-if __name__=='main':
-    print(Kospi)
