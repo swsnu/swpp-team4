2de5a63cea5f4519fbd2505ff74a17adffa0d249
CHOEJANGHYEOK <63543189+CHOEJANGHYEOK@users.noreply.github.com>
2020-11-18 01:06:04 +0900

    Jh 85 store test (#86)
    * delete unnecessary files
    
    * action and reducer test
    
    * store test lint
    
    Co-authored-by: ChanHyup98 <rlacksque@hotmail.co.kr>

--- front/src/store/reducers/user.test.js
+++ front/src/store/reducers/user.test.js
@@ -10,4 +10,19 @@ describe('LoginReducer', () => {
         const action = { type: 'RESET'};
         userReducer(undefined, action);
     })
+
+    it ('should reset submit snippet', () => {
+        const action = { type: 'RESET_SUBMIT_SNIPPET'};
+        userReducer(undefined, action);
+    })
+
+    it ('should change loading', () => {
+        const action = { type: 'CHANGE_SUBMIT_ALGORITHM'};
+        userReducer(undefined, action);
+    })
+
+    it ('should CHANGE LOADING', () => {
+        const action = { type: 'CHANGE_LOADING'};
+        userReducer(undefined, action);
+    })
 })
