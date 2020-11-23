11fd875b3308d8962935c8a16ab893931448c5fe
Hwarim <43122460+polarcoke2@users.noreply.github.com>
2020-11-23 11:22:19 +0900

    [WIP] Make Backtesting process defensive (#90)
    * test stock library
    
    * refactor wallet library
    
    * refactor stock class
    
    * docstrings
    
    * refactor wallet
    
    * refactoring backtester and sandbox
    
    * dummy commit
    
    * basic changes. Now, need to scope down the globals and locals
    
    * add  logs
    
    * add DefensiveCodeExecutor, handle importing Stock error
    
    * Now it runs
    
    * some linting issues
    
    * adding tests for wallet class
    
    * more tests
    
    * changes stated
    
    * changes - why migration error?
    
    * refine dataset seeder
    
    * address changes

--- back/qc_api/lib/wallet/stock.py
+++ back/qc_api/lib/wallet/stock.py
@@ -1,8 +1,7 @@
 """
 Stock library to manage single stock data.
 """
-from functools import reduce
-import datetime
+from datetime import datetime
 
 from typing import List, Tuple
 
@@ -12,6 +11,7 @@ class Stock:
     Stock class to manage single stock data.
     This class does not depend on user.
     """
+
     def __init__(self, name: str, stock_id: int, price: float):
         """
         Parameters:
@@ -19,31 +19,31 @@ class Stock:
             stock_id: unique id of stock
             price: (current) price of the stock
         """
-        self.name = name
-        self.stock_id = stock_id
-        self.price = price
+        self.__name = name
+        self.__stock_id = stock_id
+        self.__price = price
 
     def get_name(self) -> str:
         """Get name of Stock"""
-        return self.name
+        return self.__name
 
     def get_id(self) -> int:
         """Get Id of stock"""
-        return self.stock_id
+        return self.__stock_id
 
     def get_price(self) -> float:
         """Get current price of stock"""
-        return self.price
+        return self.__price
 
     def set_price(self, price) -> None:
         """Set price of stock"""
-        if price > 0:
-            self.price = price
+        if price >= 0:
+            self.__price = price
 
     def __str__(self):
         stock_to_dict = dict()
-        stock_to_dict.update({'name': self.name,
-                              'id': self.stock_id})
+        stock_to_dict.update({'name': self.__name,
+                              'id': self.__stock_id})
         return str(stock_to_dict)
 
 
@@ -52,13 +52,14 @@ class StockCoin(Stock):
     A Stock object stored in a wallet.
     Extended to handle user-specific information.
     """
+
     def __init__(self,
                  name: str,
                  stock_id: int,
                  price: float,
                  amount: int,
-                 purchase_log: List[Tuple[datetime.datetime, int]],
-                 sell_log: List[Tuple[datetime.datetime, int]],
+                 purchase_log: List[Tuple[datetime, float, int]],
+                 sell_log: List[Tuple[datetime, float, int]],
                  avg_purchase_price: float):
         """
         Parameters:
@@ -66,54 +67,107 @@ class StockCoin(Stock):
             stock_id: id of the stock
             price: price of the stock
             amount: the amount of the stock the user has, must be non-negative
-            purchase_log: purchase history in the form of (dateTime, amount)
+            purchase_log: purchase history in the form of (dateTime, price, amount)
             sell_log: sell history
-            avg_purchase_price: average perchase price
+            avg_purchase_price: average purchase price
         """
         super().__init__(name, stock_id, price)
-        self.amount = amount
-        self.purchase_log = purchase_log
-        self.sell_log = sell_log
-        self.avg_purchase_price = avg_purchase_price
+        self.__amount = amount
+        self.__purchase_log = purchase_log
+        self.__sell_log = sell_log
+        self.__avg_purchase_price = avg_purchase_price
+
+        if not self.check_consistency():
+            raise ValueError("Consistency failed. Please check the transaction log.")
 
     def get_amount(self) -> int:
         """Get possessed amount of the stock"""
-        return self.amount
+        return self.__amount
 
-    def sell_coin(self,
-                  date_time: datetime,
-                  amount: int) -> None:
-        """Amount checking must be done before calling this method"""
-        self.amount -= amount
-        self.sell_log.append((date_time, amount))
+    def get_purchase_log(self) -> List[Tuple[datetime, float, int]]:
+        """ Get purchase log """
+        return self.__purchase_log
 
-    def purchase_coin(self,
-                      date_time: datetime,
-                      amount: int) -> None:
-        """Budget checking must be done before calling this method"""
-        self.amount += amount
-        self.purchase_log.append((date_time, amount))
+    def get_sell_log(self) -> List[Tuple[datetime, float, int]]:
+        """ Get sell log """
+        return self.__sell_log
 
-    def fix_avg_purchase_price(self, bought_at: float, new_amount: int) -> None:
-        """update avg_purchase_rate"""
-        self.avg_purchase_price = (self.avg_purchase_price * self.amount + bought_at * new_amount) / (
-                    self.amount + new_amount)
+    def get_avg_purchase_price(self) -> float:
+        """ Get average purchase price """
+        return self.__avg_purchase_price
 
-    def check_consistency(self):
+    def __update_purchase_log(self, date: datetime, amount: int) -> None:
+        """
+        Update purchase log
+        Parameters:
+            date: transaction timestamp
+            amount: transaction amount
+        """
+        self.__purchase_log.append((date, self.get_price(), amount))
+
+    def __update_sell_log(self, date: datetime, amount: int) -> None:
+        """
+        Update sell log
+        Parameters:
+            date: transaction timestamp
+            amount: transaction amount
+        """
+        self.__sell_log.append((date, self.get_price(), amount))
+
+    def __fix_avg_purchase_price(self, new_amount: int) -> None:
+        """
+        Update avg_purchase_rate
+        """
+        self.__avg_purchase_price = (self.__avg_purchase_price * self.__amount +
+                                     self.get_price() * new_amount) / (self.__amount + new_amount)
+
+    def sell_coin(self, date_time: datetime, amount: int) -> bool:
+        """
+        Sell coin
+        Parameters:
+            date_time: transaction datetime
+            amount: transaction amount
+        Returns:
+            True if successful
+        """
+        if not self.check_consistency() or self.__amount < amount:
+            return False
+        self.__amount -= amount
+        self.__update_sell_log(date_time, amount)
+        return True
+
+    def purchase_coin(self, date_time: datetime, amount: int) -> bool:
+        """
+        Purchase coin.
+        Budget checking must be done before calling this method
+        Parameters:
+            date_time: transaction datetime
+            amount: transaction amount
+        Returns:
+            True if successful.
+        """
+        if not self.check_consistency():
+            return False
+        self.__fix_avg_purchase_price(amount)
+        self.__amount += amount
+        self.__update_purchase_log(date_time, amount)
+        return True
+
+    def check_consistency(self) -> bool:
         """
         Check if the current log is correct.
         """
-        checker = reduce(lambda x, y: x[1] + y[1], self.purchase_log) \
-                  - reduce(lambda x, y: x[1] + y[1], self.sell_log)
-
-        return self.amount == checker
+        checker = sum(x[2] for x in self.__purchase_log) - sum(x[2] for x in self.__sell_log)
+        return self.__amount == checker
 
     def __str__(self):
+        """ String representation of StockCoin """
         stock_to_dict = dict()
         # stock_to_dict.update(dict(str(super())))
-        stock_to_dict.update({'name': self.name,
-                              'amount': self.amount,
-                              'purchase_log': self.purchase_log,
-                              'sell_log': self.sell_log,
-                              'price': self.price})
+        stock_to_dict.update({'name': self.get_name(),
+                              'id': self.get_id(),
+                              'amount': self.__amount,
+                              'purchase_log': self.__purchase_log,
+                              'sell_log': self.__sell_log,
+                              'price': self.get_price()})
         return str(stock_to_dict)
--- back/qc_api/lib/wallet/wallet.py
+++ back/qc_api/lib/wallet/wallet.py
@@ -1,8 +1,7 @@
 """
 Wallet library to handle stocks possessed by the user.
 """
-import datetime
-from functools import reduce
+from datetime import datetime
 from typing import Optional, Dict, List, Any
 
 from .stock import Stock
@@ -16,33 +15,49 @@ class Wallet:
         """ default constructor """
         # WARNING: stock_id_list only has ids of stock with positive amount
         # this means that stocks which were started as amount > 0 but reduced to == 0 are not included.
-        self.budget = budget
-        self.initial_asset = budget
-        self.transaction_log = {
+        self.__budget = budget
+        self.__initial_asset = budget
+        self.__transaction_log = {
             "sell": [],
             "buy": []
         }
 
         if stock_id_to_coin is None:
-            self.stock_id_list = list()
-            self.stock_id_to_coin = dict()
+            self.__stock_id_list = list()
+            self.__id_to_coin = dict()
         else:
-            self.stock_id_to_coin = stock_id_to_coin
-            self.stock_id_list = list(map(lambda stock: stock.get_id() if stock.get_amount() > 0 else None,
-                                          [self.stock_id_to_coin[i] for i in self.stock_id_to_coin]))
+            self.__id_to_coin = stock_id_to_coin
+            self.__stock_id_list = list(map(lambda stock: stock.get_id() if stock.get_amount() > 0 else None,
+                                            [self.__id_to_coin[i] for i in self.__id_to_coin]))
 
     def get_budget(self) -> float:
         """ Get budget"""
-        return self.budget
+        return self.__budget
+
+    def get_initial_asset(self) -> float:
+        """ Get initial asset"""
+        return self.__initial_asset
 
     def get_coins_simple(self) -> List[str]:
         """ returns only 'id's of possessed stocks """
-        return self.stock_id_list
+        return self.__stock_id_list
 
     def get_coins(self) -> List[StockCoin]:
         """ returns full information of possessed stocks """
-        return [self.stock_id_to_coin[i] for i in self.stock_id_list]
+        return [self.__id_to_coin[sid] for sid in self.__stock_id_list]
         # map(lambda stock: str(stock), [self.stock_id_to_coin[i] for i in self.stock_id_to_coin])
+
+    def __handle_new_coin(self, coin: StockCoin) -> None:
+        self.__stock_id_list.append(coin.get_id())
+        self.__id_to_coin[coin.get_id()] = coin
+
+    def __handle_deleted_coin(self, coin_id: int) -> None:
+        """
+        Erase no longer possessed coin
+        Cases: price 0 or amount 0
+        """
+        self.__stock_id_list.remove(coin_id)
+        self.__id_to_coin.pop(coin_id)
 
     def update_coins(self, universe_today) -> None:
         """ updates information of possessed stocks accordingly to the today's universe"""
@@ -54,118 +69,106 @@ class Wallet:
                 coin.set_price(_coin['close'])
             except IndexError:
                 coin.set_price(0)
-                self.stock_id_list.remove(coin.get_id())
+                self.__handle_deleted_coin(coin.get_id())
 
-    def sell_coin(self,
-                  stock: Stock,
-                  amount: int,
-                  time: datetime.date) -> bool:
+    def sell_coin(self, stock: Stock, amount: int, time: datetime) -> bool:
         """ Sell stock """
         stock_id = stock.get_id()
-        if amount == 0 or stock_id not in self.stock_id_to_coin.keys():
+        if amount <= 0 or stock_id not in self.__stock_id_list:
             return False
 
-        stock_coin = self.stock_id_to_coin[stock_id]
+        stock_coin = self.__id_to_coin[stock_id]
         possessed_amount = stock_coin.get_amount()
 
         if possessed_amount < amount:
             return False
         if possessed_amount == amount:
-            self.budget += stock.get_price() * amount
-            stock_coin.sell_coin(time, amount)
-            self.stock_id_list.remove(stock_id)
-            self.transaction_log.get("sell").append({
-                "name": stock.get_name(),
-                "price": stock.get_price(),
-                "amount": amount
-            })
-            print(f'Sold {amount} {stock.get_name()} at price of {stock.get_price()}')
-        else:
-            self.budget += stock.get_price() * amount
-            stock_coin.sell_coin(time, amount)
-            self.transaction_log.get("sell").append({
-                "name": stock.get_name(),
-                "price": stock.get_price(),
-                "amount": amount
-            })
-            print(f'Sold {amount} {stock.get_name()} at price of {stock.get_price()}')
+            self.__handle_deleted_coin(stock_id)
+        self.__budget += stock.get_price() * amount
+        stock_coin.sell_coin(time, amount)
+        self.__transaction_log.get("sell").append({
+            "name": stock.get_name(),
+            "price": stock.get_price(),
+            "amount": amount
+        })
+        print(f'Sold {amount} {stock.get_name()} at price of {stock.get_price()}')
         return True
 
-    def purchase_coin(self,
-                      stock: Stock,
-                      amount: int,
-                      time: datetime):
+    def purchase_coin(self, stock: Stock, amount: int, time: datetime) -> bool:
         """ Purchase stocks """
         stock_id = stock.get_id()
-        if self.budget < stock.get_price() * amount or amount == 0:
+        print(stock, amount)
+        if self.__budget < stock.get_price() * amount or amount <= 0:
             print("cannot buy")
             return False
 
-        if stock_id not in self.stock_id_to_coin:
-            stock_coin = StockCoin(stock.get_name(), stock_id, stock.get_price(),
-                                   amount, list(), list(), stock.get_price())
-            self.stock_id_to_coin.update({stock_id: stock_coin})
-            self.budget -= stock.get_price() * amount
-            # stock_coin.purchase_coin(time, amount)
-            self.stock_id_list.append(stock_id)
-            print(f'Bought {amount} {stock.get_name()} at price {stock.get_price()}')
-            self.transaction_log.get("buy").append({
-                "name": stock.get_name(),
-                "price": stock.get_price(),
-                "amount": amount
-            })
+        if stock_id not in self.__stock_id_list:
+            stock_coin = StockCoin(name=stock.get_name(), stock_id=stock_id, price=stock.get_price(),
+                                   amount=0, purchase_log=list(), sell_log=list(), avg_purchase_price=0)
+            self.__handle_new_coin(stock_coin)
         else:
-            stock_coin = self.stock_id_to_coin[stock_id]
-            self.budget -= stock.get_price() * amount
-            stock_coin.fix_avg_purchase_price(bought_at=stock.get_price(), new_amount=amount)
-            if stock_coin.get_amount() == 0:
-                self.stock_id_list.append(stock_id)
-            stock_coin.purchase_coin(time, amount)
-            print(f'Bought {amount} {stock.get_name()} at price {stock.get_price()}')
-            self.transaction_log.get("buy").append({
-                "name": stock.get_name(),
-                "price": stock.get_price(),
-                "amount": amount
-            })
+            stock_coin = self.__id_to_coin[stock_id]
+        self.__budget -= stock.get_price() * amount
+        stock_coin.purchase_coin(date_time=time, amount=amount)
+        print(f'Bought {amount} {stock.get_name()} at price {stock.get_price()}')
+        self.__transaction_log.get("buy").append({
+            "name": stock.get_name(),
+            "price": stock.get_price(),
+            "amount": amount
+        })
         return True
 
     def get_summaries(self) -> Dict[str, Any]:
         """ Calculates the statistics of the current status and returns it."""
+        possessed_items = [
+            (self.__id_to_coin[index].get_name(), self.__id_to_coin[index].get_amount())
+            for index in self.__stock_id_list
+        ]
+        total_profit = sum([self.__id_to_coin[index].get_avg_purchase_price() *
+                            self.__id_to_coin[index].get_amount() for index in self.__stock_id_list],
+                           self.get_budget())
+        total_profit = ((total_profit / self.__initial_asset) - 1) * 100
+        stock_profit = sum([self.__id_to_coin[index].get_avg_purchase_price() *
+                            self.__id_to_coin[index].get_amount() for index in self.__stock_id_list], 0)
         summaries = {}
         summaries.update({"cash": self.get_budget()})
-        summaries.update({"posessed_items": [
-            (self.stock_id_to_coin[index].name, self.stock_id_to_coin[index].get_amount()) for index in
-            self.stock_id_list]})
-        summaries.update(
-            {"total_profit": (((reduce(lambda a, b: a + b, [
-                self.stock_id_to_coin[index].avg_purchase_price * self.stock_id_to_coin[index].amount for index in
-                self.stock_id_list
-            ], 0) + self.get_budget()) / self.initial_asset) - 1) * 100
-             })
-        summaries.update({"stock_profit": reduce(lambda a, b: a + b, [
-            self.stock_id_to_coin[index].avg_purchase_price * self.stock_id_to_coin[index].amount for index in
-            self.stock_id_list
-        ], 0)})
+        summaries.update({"possessed_items": possessed_items})
+        summaries.update({"total_profit": total_profit})
+        summaries.update({"stock_profit": stock_profit})
         return summaries
 
-    def get_profit(self):
+    def get_profit(self) -> float:
+        """
+        Calculate and return profit.
+        Returns:
+            profit.
+        """
         remaining_budget = self.get_budget()
-        possessed_stock_value = reduce(lambda a, b: a + b, [
-                self.stock_id_to_coin[index].get_price() * self.stock_id_to_coin[index].amount for index in
-                self.stock_id_list
-            ], 0)
-        profit = 100*(remaining_budget+possessed_stock_value)/self.initial_asset
+        possessed_stock_value = sum([self.__id_to_coin[index].get_price() * self.__id_to_coin[index].get_amount()
+                                     for index in self.__stock_id_list], 0)
+        profit = 100 * (remaining_budget + possessed_stock_value) / self.__initial_asset
         return profit
 
-    def get_transaction_log(self, date):
-        rv = {}
-        rv.update(self.transaction_log)
-        rv.update({'date': str(date)})
-        self.transaction_log = {
+    def get_transaction_log(self, date: datetime) -> Dict[str, Any]:
+        """
+        Get transaction log of the queried day and empty current log.
+        Parameters:
+            date: date to query
+        Returns:
+            transaction log of the day
+        """
+        rv = dict()
+        rv.update(self.__transaction_log)
+        rv['date'] = str(date)
+        self.__transaction_log = {
             "sell": [],
             "buy": []
         }
         return rv
 
+    def __repr__(self):
+        return [str(coin) for coin in self.get_coins()]
+
     def __str__(self):
-        return str(self.get_coins())
+        """ String representation of wallet. """
+        return f'{self.__repr__()}'
